67a49fade906fa5d371542806bbaba998bf9981c
Mathieudm <Mathieu.De-Malibran@ensimag.grenoble-inp.fr>
2014-06-03 01:02:20 +0200

    creation projet

--- XdataFiWS/HelloServiceExemple/ActifService/ActifService/ActifService.cs
+++ XdataFiWS/HelloServiceExemple/ActifService/ActifService/ActifService.cs
@@ -32,5 +32,7 @@ namespace ActifService
 
             return code;
         }
+
+        
     }
 }
---
+++ XdataFiWS/Projet/Service/ProjetService/ProjetService/ProjetService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ProjetService
+{
+    // REMARQUE : vous pouvez utiliser la commande Renommer du menu Refactoriser pour changer le nom de classe "ProjetService" à la fois dans le code et le fichier de configuration.
+    public class ProjetService : IProjetService
+    {
+        public void DoWork()
+        {
+        }
+    }
+}
