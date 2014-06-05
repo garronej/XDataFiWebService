6288ff1b991949343a4780478d7e13117df4bd43
Mathieudm <Mathieu.De-Malibran@ensimag.grenoble-inp.fr>
2014-06-05 09:24:18 +0200

    modif affichage wpf

---
+++ XdataFiWS/Projet/WPFClient/WpfApplication1/WpfApplication1/SampleData/Database/Database.xaml.cs
@@ -0,0 +1,105 @@
+﻿//      *********    NE PAS MODIFIER CE FICHIER     *********
+//      Ce fichier est régénéré par un outil de création.Modifier
+// .     ce fichier peut provoquer des erreurs.
+namespace Expression.Blend.SampleData.Database
+{
+	using System; 
+	using System.ComponentModel;
+
+// Pour réduire de façon significative le volume des données échantillons dans votre application de production, vous pouvez définir
+// la constante de compilation conditionnelle DISABLE_SAMPLE_DATA et désactiver les données échantillons lors de l'exécution.
+#if DISABLE_SAMPLE_DATA
+	internal class Database { }
+#else
+
+	public class Database : INotifyPropertyChanged
+	{
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		protected virtual void OnPropertyChanged(string propertyName)
+		{
+			if (this.PropertyChanged != null)
+			{
+				this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+			}
+		}
+
+		public Database()
+		{
+			try
+			{
+				Uri resourceUri = new Uri("/WpfApplication1;component/SampleData/Database/Database.xaml", UriKind.RelativeOrAbsolute);
+				System.Windows.Application.LoadComponent(this, resourceUri);
+			}
+			catch
+			{
+			}
+		}
+
+		private ItemCollection _Collection = new ItemCollection();
+
+		public ItemCollection Collection
+		{
+			get
+			{
+				return this._Collection;
+			}
+		}
+	}
+
+	public class Item : INotifyPropertyChanged
+	{
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		protected virtual void OnPropertyChanged(string propertyName)
+		{
+			if (this.PropertyChanged != null)
+			{
+				this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+			}
+		}
+
+		private string _Property1 = string.Empty;
+
+		public string Property1
+		{
+			get
+			{
+				return this._Property1;
+			}
+
+			set
+			{
+				if (this._Property1 != value)
+				{
+					this._Property1 = value;
+					this.OnPropertyChanged("Property1");
+				}
+			}
+		}
+
+		private System.Windows.Media.ImageSource _Property2 = null;
+
+		public System.Windows.Media.ImageSource Property2
+		{
+			get
+			{
+				return this._Property2;
+			}
+
+			set
+			{
+				if (this._Property2 != value)
+				{
+					this._Property2 = value;
+					this.OnPropertyChanged("Property2");
+				}
+			}
+		}
+	}
+
+	public class ItemCollection : System.Collections.ObjectModel.ObservableCollection<Item>
+	{ 
+	}
+#endif
+}
