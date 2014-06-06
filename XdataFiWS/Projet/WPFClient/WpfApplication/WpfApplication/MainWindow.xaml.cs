428df72e025532c5c324c31b73768fb25e6b2e63
Mathieudm <Mathieu.De-Malibran@ensimag.grenoble-inp.fr>
2014-06-06 18:44:56 +0200

    modif wpf

--- XdataFiWS/Projet/WPFClient/WpfApplication/WpfApplication/MainWindow.xaml.cs
+++ XdataFiWS/Projet/WPFClient/WpfApplication/WpfApplication/MainWindow.xaml.cs
@@ -12,17 +12,47 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Collections.Generic;
 
 namespace WpfApplication
 {
     /// <summary>
     /// Logique d'interaction pour MainWindow.xaml
     /// </summary>
+    
     public partial class MainWindow : Window
     {
+        List<String> Item = new List<String>();
+       
         public MainWindow()
         {
+            Item.Add("EUR");
+            Item.Add("USD");
             InitializeComponent();
+            
+			
+            
+            
+			
+        }
+
+        private void Button_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+        	
+			int IdDevise2 = Devise2.SelectedIndex;
+           
+            String Date2 = DateFin.SelectedDate.ToString();
+
+            
+
+//XdataFiWebService.HelloWcfServiceClient client = new XdataFiWebService.HelloWcfServiceClient();
+
+            //client.Currency Devise1 = client.Currency(Devise1.SelectedValue.ToString());
+            //DateTime Date1 = Convert.ToDateTime(DateDebut.SelectedDate.ToString());
+           
+            //client.devise()
+			
+			
         }
     }
 }
