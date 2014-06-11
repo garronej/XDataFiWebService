a41eea8243f745a44f86f2fa7df30e87d687a02a
KevinRecuerda <Kevin.Recuerda@ensimag.grenoble-inp.fr>
2014-06-11 09:57:20 +0200

    WPF

--- XdataFiWS/Projet/WpfXDataFi/WpfXDataFi/Content/TauxDeChange.xaml.cs
+++ XdataFiWS/Projet/WpfXDataFi/WpfXDataFi/Content/TauxDeChange.xaml.cs
@@ -10,6 +10,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WpfXDataFi.ServiceReference;
 
 namespace WpfXDataFi
 {
@@ -24,6 +25,47 @@ namespace WpfXDataFi
 		{
 			this.InitializeComponent();
             this.mw = mw;
+			
+			foreach (Data.Currency cur in (Data.Currency[])Enum.GetValues(typeof(Data.Currency))) {
+            
+                Devise1.Items.Add(cur);
+                Devise2.Items.Add(cur);
+            } 
+
+            DateDebut.SelectedDate = DateTime.Today;
+            DateFin.SelectedDate = DateTime.Today;
 		}
+		
+		 private void EnterTauxChange_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+			// Récupération des devises
+            Data.Currency currencyToCompare = (Data.Currency)
+                Enum.Parse(typeof(Data.Currency), Devise1.SelectedValue.ToString());
+
+			Data.Currency currency2 = (Data.Currency)
+                Enum.Parse(typeof(Data.Currency), Devise2.SelectedValue.ToString());
+			
+            List<Data.Currency> l = new List<Data.Currency>();
+            l.Add(currency2);
+
+			// Récupération des dates
+            DateTime debut = Convert.ToDateTime(DateDebut.SelectedDate.ToString());
+            DateTime fin = Convert.ToDateTime(DateFin.SelectedDate.ToString());
+			
+			// Traitement des données
+            try
+            {
+                ServiceReference.ExchangeRateServiceClient client = new ServiceReference.ExchangeRateServiceClient();
+                DataExchangeRate d = client.getExchangeRate(currencyToCompare, l, debut, fin, Data.Frequency.Monthly);
+                client.Close();
+				
+				mw.showRes(d);
+            }
+            catch (Exception ex)
+            {
+                //TO DO ouvrir une page d'erreur pour demander au client de recommencer la manoeuvre
+                Console.WriteLine("Une erreur s'est produite");
+            }
+        }
 	}
 }
